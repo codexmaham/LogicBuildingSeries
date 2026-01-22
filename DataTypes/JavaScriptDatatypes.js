3c0f2e18b9470d266f010c842cef4e1c25f0e3dc
Syeda Maham Fatima <codexmaham@gmail.com>
2026-01-22 15:35:01 +0500

    DT, V

---
+++ DataTypes/JavaScriptDatatypes.js
@@ -0,0 +1,26 @@
+// Types of Datatypes in Js
+// 1. Primitive
+// 2. Non- primituve
+
+// Primitive Datatypes
+// Number
+// Boolean
+// undefined
+// String
+// Symbol
+// BigInt
+// object 
+
+// Non Primitive
+// Arrays
+// objects
+// function  
+
+//to check types of the variables
+typeof "Ali"; // "string"
+typeof 123; // "number"
+typeof true; // "boolean"
+typeof undefined; // "undefined"
+typeof null; // "object" (quirk in JS)
+typeof [1, 2, 3]; // "object"
+typeof function () {}; // "function"
---
+++ DataTypes/Variables.js
@@ -0,0 +1,11 @@
+//can't reassign, block scope
+const name = "Maham"
+
+//can reassign, block scope
+let age = 27
+age = 21
+
+//older version, function scope
+var Fname = "father"
+
+
